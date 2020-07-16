7abac623b4fc0cbefdcaeda4aff1f952b616d466
Tejal Badgujar <57011203+tejalbadgujar@users.noreply.github.com>
2020-07-17 02:24:01 +0530

    assignments

---
+++ JSAssD3B1/scripts/evenOdd.js
@@ -0,0 +1,13 @@
+let num = prompt("Enter a number : ");
+        function oddEven(no) {
+            if (no%2 == 0) {
+                console.log(`The number entered is  ${no}  and Number is Even.`);
+            }
+            else {
+                console.log(`The number entered is  ${no}  and Number is Odd.`);
+            }
+        }
+
+let result = oddEven(num);
+
+console.log(result);
---
+++ JSAssD3B1/scripts/osNameVersion.js
@@ -0,0 +1,4 @@
+let name = prompt("Enter Operating System name : ", "Android");
+let version = prompt("Enter version : ", 9);
+
+console.log(`The OS name is ${name} and version is ${version}`);
